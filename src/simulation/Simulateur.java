5e83dd354378f1b0d52563ffd8f067eb35497592
stobr <stobr@10.182.80.153>
2020-12-01 08:56:55 +0100

    test

--- src/simulation/Simulateur.java
+++ src/simulation/Simulateur.java
@@ -16,7 +16,7 @@ public class Simulateur implements Runnable {
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				e.printStackTrace(); 
 			}
 		}
 	}
