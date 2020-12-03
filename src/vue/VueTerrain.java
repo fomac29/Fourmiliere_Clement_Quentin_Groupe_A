31c5f0819b2bd5d832e3805d71f5aac39a5a51bc
Stoliaroff <Clement>
2020-12-03 14:39:25 +0100

    javadoc VueTerrain

--- src/vue/VueTerrain.java
+++ src/vue/VueTerrain.java
@@ -8,40 +8,64 @@ import graphicLayer.GOval;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
+/**
+ * Représentation graphique du terrain dans lequel les fourmis peuvent se déplacer ainsi que la
+ * fenêtre d'application
+ * 
+ * @author Clément Stoliaroff, Quentin Tassy
+ */
 public class VueTerrain {
-  
+  /**
+   * Hauteur du rectangle qui représente le terrain
+   */
   private final int hauteurTerrain = 500;
+
+  /**
+   * Largeur du rectangle qui représente le terrain
+   */
   private final int largeurTerrain = 500;
+
+  /**
+   * Représente la fenêtre d'application
+   */
   private GSpace fenetre;
+
+  /**
+   * Représente le rectangle représentant le terrain qui contiendra les éléments graphiques
+   * (fourmis, proies, etc...)
+   */
   private GBounded terrain;
-  
+
+  /**
+   * 
+   */
   public VueTerrain() {
     this.InitialiserVue();
   }
-  
+
   public void InitialiserVue() {
     fenetre = new GSpace("Terrain", new Dimension(550, 550));
     fenetre.open();
   }
-  
+
   public void ajouterTerrain() {
     terrain = new GBounded();
     terrain.setColor(Color.green);
     terrain.setPosition(new Point(30, 30));
     terrain.setDimension(new Dimension(hauteurTerrain, largeurTerrain));
-    
+
     fenetre.addElement(terrain);
   }
-  
+
   public void ajouterFourmiliere(GRect composantGraphiqueFourmiliere) {
     this.terrain.addElement(composantGraphiqueFourmiliere);
   }
-  
+
   public void ajouterFourmi(GOval composantGraphiqueFourmi) {
     this.terrain.addElement(composantGraphiqueFourmi);
     composantGraphiqueFourmi.repaint();
   }
-  
+
   public void supprimerFourmi(GOval composantGraphiqueFourmi) {
     this.terrain.removeElement(composantGraphiqueFourmi);
   }
